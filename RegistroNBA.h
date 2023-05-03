28eb746b9152e2dc5eac5f0025b94509e8cc9cfc
PachoRyan <112034734+PachoRyan@users.noreply.github.com>
2023-05-02 23:29:17 -0500

    RegistroNBA & RegistroTornado with Functions
    Unitary & Vec functions

--- RegistroNBA.h
+++ RegistroNBA.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include<vector>
+#include<fstream>
 struct RegistroNBA
 {
     // KeyType must be a type that can be compared using >, < and ==. 
@@ -25,5 +28,61 @@ struct RegistroNBA
 
 RegistroNBA RegNBAfromCSVline(std::string tuple)
 {
-    // esta funcion recibe algo como (<values>) y retorna un registro completo
+    RegistroNBA reg;
+
+    std::vector<std::string> attrs;
+    std::string save = "";
+    int contador =-1;
+    for(auto i:tuple){
+        contador++;
+        if(contador != tuple.size()-1) {
+            if (i != ',') {
+                save += i;
+            } else {
+                attrs.push_back(save);
+                save = "";
+            }
+        }else{
+            save += i;
+            attrs.push_back(save);
+            save = "";
+        }
+    }
+
+    for (int x = 0; x < attrs[0].size(); x++) {
+        reg.home_team[x] = attrs[0][x];
+    }
+
+    reg.matchup_id = stol(attrs[1]);
+
+    reg.home_points = stol(attrs[2]);
+
+    for (int y = 0; y < attrs[3].size(); y++) {
+        reg.away_team[y] = attrs[3][y];
+    }
+
+    reg.away_points = stol(attrs[4]);
+
+
+    return reg;
+}
+
+std::vector<RegistroNBA> NBAFromCsvToVec(std::string filename) {
+    RegistroNBA regtovec;
+    std::vector<RegistroNBA> vecfinal;
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+
+    if (file.fail()) {
+        exit(1);
+    }
+
+    std::string datatotal = "";
+
+    while(getline(file, datatotal)){
+        regtovec = RegNBAfromCSVline(datatotal);
+        vecfinal.push_back(regtovec);
+    }
+
+    return vecfinal;
 }
--- RegistroTornados.h
+++ RegistroTornados.h
@@ -1,13 +1,66 @@
 #pragma once
-
+#include<string>
+#include<vector>
+#include<fstream>
 struct RegistroTornados
 {
-    char date[11];
-    char state[2];
+    char date[11]{0};
+    char state[3]{0};
     long magnitude;
 };
 
-RegistroNBA TornadosFromCSVline(std::string tuple)
+RegistroTornados TornadosFromCSVline(std::string tuple)
 {
-    // esta funcion recibe algo como (<values>) y retorna un registro completo
+    RegistroTornados reg;
+
+    std::vector<std::string> attrs;
+    std::string save = "";
+    int contador =-1;
+    for(auto i:tuple){
+        contador++;
+        if(contador != tuple.size()-1) {
+            if (i != ',') {
+                save += i;
+            } else {
+                attrs.push_back(save);
+                save = "";
+            }
+        }else{
+            save += i;
+            attrs.push_back(save);
+            save = "";
+        }
+    }
+
+    for (int x = 0; x < attrs[0].size(); x++) {
+        reg.date[x] = attrs[0][x];
+    }
+    for (int y = 0; y < attrs[1].size(); y++) {
+        reg.state[y] = attrs[1][y];
+    }
+    reg.magnitude = stol(attrs[2]);
+
+    return reg;
+
+}
+
+std::vector<RegistroTornados> TornadosFromCSVtovec(std::string filename){
+    RegistroTornados regtovec;
+    std::vector<RegistroTornados> vecfinal;
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+
+    if (file.fail()) {
+        exit(1);
+    }
+
+    std::string datatotal = "";
+
+    while(getline(file, datatotal)){
+        regtovec = TornadosFromCSVline(datatotal);
+        vecfinal.push_back(regtovec);
+    }
+
+    return vecfinal;
+
 }
